125cee9a18b2c535d3731d6f8842d278f1958f3f
Renan Scarpato <99297914+rscarpato@users.noreply.github.com>
2023-03-29 18:37:48 -0300

    Update listaProdutos.php
    corrigido bug: 'id' substituido por 'id_produto'
    adicionado botão de deletar em cada item da lista

--- Projeto v1.0/html/listaProdutos.php
+++ Projeto v1.0/html/listaProdutos.php
@@ -13,7 +13,7 @@ $resultado_produtos = $conexao->query($sql_produtos);
 $produtos = [];
 if ($resultado_produtos->num_rows > 0) {
   while ($linha = $resultado_produtos->fetch_assoc()) {
-    $id_produto = $linha['id'];
+    $id_produto = $linha['id_produto'];
     $sql_imagens = "SELECT * FROM imagem WHERE id_produto = $id_produto";
     $resultado_imagens = $conexao->query($sql_imagens);
     $imagens = [];
@@ -25,6 +25,31 @@ if ($resultado_produtos->num_rows > 0) {
     $linha['imagens'] = $imagens;
     $produtos[] = $linha;
   }
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $id_produto = $_POST['id_produto'];
+
+  // Deleta o produto e suas imagens
+  $sql_imagens = "SELECT * FROM imagem WHERE id_produto = $id_produto";
+  $resultado_imagens = $conexao->query($sql_imagens);
+  if ($resultado_imagens->num_rows > 0) {
+    while ($linha = $resultado_imagens->fetch_assoc()) {
+      unlink($linha['caminho']);
+    }
+  }
+  
+  $sql_deletar_imagens = "DELETE FROM imagem WHERE id_produto = $id_produto";
+  $conexao->query($sql_deletar_imagens);
+
+  $sql_deletar_produto = "DELETE FROM produto WHERE id_produto = $id_produto";
+ 
+  $conexao->query($sql_deletar_produto);
+
+  // Redireciona para a página de listagem de produtos
+  header('Location: listaProdutos.php');
+
+
+  exit();
 }
 ?>
 
@@ -54,7 +79,7 @@ if ($resultado_produtos->num_rows > 0) {
       <tbody>
         <?php foreach ($produtos as $produto) : ?>
           <tr>
-            <td><?php echo $produto['id'] ?></td>
+            <td><?php echo $produto['id_produto'] ?></td>
             <td><?php echo $produto['nome'] ?></td>
             <td><?php echo $produto['descricao'] ?></td>
             <td><?php echo $produto['preco'] ?></td>
@@ -63,6 +88,12 @@ if ($resultado_produtos->num_rows > 0) {
               <?php foreach ($produto['imagens'] as $imagem) : ?>
                 <img src="<?php echo $imagem ?>" alt="Imagem do Produto">
               <?php endforeach ?>
+            </td>
+            <td>
+              <form action="" method="POST">
+                <input type="hidden" name="id_produto" value="<?php echo $produto['id_produto']; ?>">
+                <button type="submit">Deletar</button>
+              </form>
             </td>
           </tr>
         <?php endforeach ?>
