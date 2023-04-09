fafc7178b4d9648796a1e58d963b53942eaf3709
Slipvic <92764645+Slipvic@users.noreply.github.com>
2023-04-08 23:56:13 -0300

    Sprint 2
    As unicas pendencias restantes são 
    -alterar o usuário (o id está sendo passado pela url)
    -integrar a pagina de detalhe do produto

---
+++ Sistema Web/scriptBD.sql
@@ -0,0 +1,42 @@
+create database artgallery;
+use artgallery;
+CREATE TABLE `users` (
+  `idusers` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(110) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `sexo` varchar(45) NOT NULL,
+  PRIMARY KEY (`idusers`)
+);
+CREATE TABLE `artgallery`.`funcionarios` (
+  `idfuncionarios` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(110) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `cpf` VARCHAR(14) NOT NULL,
+  `senha` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idfuncionarios`));
+  
+  ALTER TABLE funcionarios ADD grupo VARCHAR(45);
+  
+CREATE TABLE produto (
+  id_produto INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(200) NOT NULL,
+  avaliacao DECIMAL(2,1) NOT NULL,
+  descricao TEXT NOT NULL,
+  preco DECIMAL(10,2) NOT NULL,
+  qtd_estoque INT NOT NULL
+);
+
+CREATE TABLE imagem (
+  id_imagem INT PRIMARY KEY AUTO_INCREMENT,
+  id_produto INT NOT NULL,
+  caminho VARCHAR(200) NOT NULL,
+  eh_padrao BOOLEAN NOT NULL,
+  FOREIGN KEY (id_produto) REFERENCES produto(id_produto) ON DELETE CASCADE
+);
+
+
+
+SELECT * FROM artgallery.users;
+SELECT * FROM artgallery.funcionarios;
+/*drop database loja;
