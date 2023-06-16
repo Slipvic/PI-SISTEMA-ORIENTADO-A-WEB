aad0e00f01e6f0e0f916c46480ac6127cbacf2b4
Bruno Pinheiro <bpinheiro@autodoc.local>
2023-06-16 15:26:35 -0300

    Alteração na tela de resumo

--- Projeto v1.0/html/formaPagamento.php
+++ Projeto v1.0/html/formaPagamento.php
@@ -13,9 +13,6 @@ if (!isset($_SESSION['idusers'])) {
 $id = $_SESSION['idusers'];
 $sql = "SELECT id, logradouro, numero, uf FROM endereco WHERE idusers = $id";
 $result = $conexao->query($sql);
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -34,225 +31,107 @@ $result = $conexao->query($sql);
         .selected {
             border: 2px solid blue;
         }
+
+        .navbar {
+            background-color: #7c4dff;
+            color: #fff;
+            font-weight: bold;
+        }
+
+        .navbar-brand {
+            font-size: 1.5rem;
+        }
+
+        .nav-link {
+            color: #fff;
+            font-size: 1.2rem;
+            margin-right: 1rem;
+        }
     </style>
 </head>
 
 <body>
 
-   <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Ecommerce de Artes</a>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="indexClientes.php">Ecommerce de Artes</a>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="carrinho.php">Carrinho</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="meusPedidos.php">Meus Pedidos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="indexClientes.php">Quadros</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link"
-            href="<?php echo isset($_SESSION['idusers']) ? 'perfilCliente.php' : 'login-client.php'; ?>">
-            <?php echo isset($_SESSION['idusers']) ? 'Perfil' : 'Login'; ?>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="carrinho.php">Carrinho</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo isset($_SESSION['idusers']) ? 'meusPedidos.php' : 'carrinho.php'; ?>">
+                        <?php echo isset($_SESSION['idusers']) ? 'Meus Pedidos' : 'Meus Pedidos'; ?>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="indexClientes.php">Quadros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo isset($_SESSION['idusers']) ? 'perfilCliente.php' : 'login-client.php'; ?>">
+                        <?php echo isset($_SESSION['idusers']) ? 'Perfil' : 'Login'; ?>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container mt-5 d-flex justify-content-center align-items-center">
+    <div class="col-md-6">
+        <div class="card p-5">
+            <h4 class="text-center">Resumo da Compra</h4>
+            <hr>
+            <p>Produtos:</p>
+            <ul>
+                <?php
+                // Verifica se os detalhes do carrinho estão armazenados na sessão
+                if (isset($_SESSION['detalhes_carrinho'])) {
+                    $detalhes_carrinho = $_SESSION['detalhes_carrinho'];
 
-    <div class="container mt-5">
-        <div class="row">
+                    // Acessa os dados gerais do carrinho
+                    $total_carrinho = $detalhes_carrinho['total_carrinho'];
+                    $itens_carrinho = $detalhes_carrinho['itens_carrinho'];
 
-            <!-- Painel de endereço -->
-            <div class="col-md-6">
-                <div class="card p-4">
-                    <h4>Endereços de Entrega</h4>
-                    <hr>
-                    <?php
-                    // Verifica se a consulta SQL retornou algum resultado
-                    if ($result->num_rows > 0) {
-                        // Exibe os endereços do usuário
-                        while ($row = $result->fetch_assoc()) {
-                            $logradouro = $row['logradouro'];
-                            $numero = $row['numero'];
-                            $uf = $row['uf'];
+                    // Exibe os detalhes de cada item do carrinho
+                    foreach ($itens_carrinho as $item) {
+                        $nome_produto = $item['nome_produto'];
+                        $valor_sem_frete = $item['valor_sem_frete'];
+                        $frete = $item['frete'];
+                        $quantidade = $item['quantidade'];
 
-                            echo '<div class="form-check">';
-                            echo '<input class="form-check-input" type="radio" name="endereco" id="endereco' . $numero . '">';
-                            echo '<label class="form-check-label" for="endereco' . $numero . '">';
-                            echo $logradouro . ', ' . $numero . ' - ' . $uf;
-                            echo '</label>';
-                            echo '</div>';
-                        }
+                        echo "<p>Nome do quadro: $nome_produto</p>";
+                        echo "<p>Valor unitário: R$ $valor_sem_frete</p>";
+                        echo "<p>Frete: $frete</p>";
+                        echo "<p>Quantidade: $quantidade</p>"; // Exibe a quantidade selecionada do item
+                        echo "<hr>"; // Adicione uma linha para separar cada item do carrinho
                     }
-                    ?>
-                </div>
+                }
+                ?>
+            </ul>
+            <p class="text-center">
+                <?php
+                // Exibe o total do carrinho
+                echo "<h3>Valor total: R$ $total_carrinho</h3>";
+                ?>
+            </p>
+            <div class="text-center mt-4">
+                <a href="formaPagamentoEndereco.php" class="btn btn-primary">Selecionar Endereço e Pagamento</a>
             </div>
-            <div class="col-md-6">
-                <div class="card p-4">
-                    <h4>Opções de Pagamento</h4>
-                    <hr>
-                    <div class="payment-option" id="option1">
-                        <h5>Cartão de Crédito</h5>
-                        <p>Informe os detalhes do cartão de crédito.</p>
-                    </div>
-                    <div class="payment-option mt-3" id="option2">
-                        <h5>Boleto Bancário</h5>
-                        <p>Selecione essa opção para gerar um boleto bancário.</p>
-                    </div>
-                    <div class="payment-option mt-3" id="option3">
-                        <h5>Pix</h5>
-                        <p>Realize um pix através do nosso QR Code e efetue o pagamento em instantes.</p>
-                    </div>
-                    <button class="btn btn-primary mt-4" id="payButton">Pagar</button>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card p-5">
-                    <h4>Resumo da Compra</h4>
-                    <hr>
-                    <p>Produtos:</p>
-                    <ul>
-                        <?php
-                        // Verifica se os detalhes do carrinho estão armazenados na sessão
-                        if (isset($_SESSION['detalhes_carrinho'])) {
-                            $detalhes_carrinho = $_SESSION['detalhes_carrinho'];
-
-                            // Acessa os dados gerais do carrinho
-                            $total_carrinho = $detalhes_carrinho['total_carrinho'];
-                            $itens_carrinho = $detalhes_carrinho['itens_carrinho'];
-
-                            // Exibe os detalhes de cada item do carrinho
-                            foreach ($itens_carrinho as $item) {
-                                $nome_produto = $item['nome_produto'];
-                                $valor_sem_frete = $item['valor_sem_frete'];
-                                $frete = $item['frete'];
-                                $quantidade = $item['quantidade'];
-
-                                echo "<p>Nome do quadro: $nome_produto</p>";
-                                echo "<p>Valor unitário: R$ $valor_sem_frete</p>";
-                                echo "<p>Frete: $frete</p>";
-                                echo "<p>Quantidade: $quantidade</p>"; // Exibe a quantidade selecionada do item
-                                echo "<hr>"; // Adicione uma linha para separar cada item do carrinho
-                            }
-                        }
-                        ?>
-                    </ul>
-                    <p>
-                        <?php
-                        // Exibe o total do carrinho
-                        echo "<h3>Valor total: R$ $total_carrinho</h3>";
-                        ?>
-                    </p>
-                </div>
+            <div class="text-center mt-4">
+                <a href="carrinho.php" class="btn btn-primary">Modificar Pedido</a>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- Modal para preencher informações do cartão de crédito -->
-    <div class="modal fade" id="cartaoModal" tabindex="-1" role="dialog" aria-labelledby="cartaoModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="cartaoModalLabel">Preencha as informações do cartão de crédito</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form id="cartaoForm" action="processar_pagamento.php" method="POST">
-                        <div class="form-group">
-                            <label for="nomeCartao">Nome no cartão</label>
-                            <input type="text" class="form-control" id="nomeCartao" name="nomeCartao" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="numCartao">Número do cartão</label>
-                            <input type="text" class="form-control" id="numCartao" name="numCartao" required>
-                        </div>
-                        <div class="form-row">
-                            <div class="col">
-                                <label for="validade">Validade</label>
-                                <input type="text" class="form-control" id="validade" name="validade" required>
-                            </div>
-                            <div class="col">
-                                <label for="cvv">CVV</label>
-                                <input type="text" class="form-control" id="cvv" name="cvv" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group mt-3">
-                            <label for="parcelas">Número de parcelas</label>
-                            <select class="form-control" id="parcelas" name="parcelas">
-                                <option value="1">À vista</option>
-                                <option value="2">2 vezes</option>
-                                <option value="3">3 vezes</option>
-                                <option value="4">4 vezes</option>
-                            </select>
-                        </div>
-                        <button type="submit" class="btn btn-primary mt-4">Pagar</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
-    <script>
-        $(document).ready(function () {
-            // Adiciona a classe "selected" ao clicar na opção de pagamento
-            $('.payment-option').click(function () {
-                $('.payment-option').removeClass('selected');
-                $(this).addClass('selected');
-            });
-
-            // Abre o modal de preenchimento do cartão de crédito ao clicar no botão "Pagar"
-            $('#payButton').click(function () {
-                var selectedOption = $('.payment-option.selected').attr('id');
-
-                switch (selectedOption) {
-                    case 'option1':
-                        $('#cartaoModal').modal('show');
-                        break;
-                    case 'option2':
-                        window.location.href = 'pagamento_boleto.php';
-                        break;
-                    case 'option3':
-                        window.location.href = 'pagamento_pix.php';
-                        break;
-                    default:
-                        break;
-                }
-            });
-
-            // Envia o formulário do cartão de crédito para processar o pagamento
-            $('#cartaoForm').submit(function (event) {
-                event.preventDefault(); // Impede o envio do formulário por padrão
-
-                // Aqui você pode adicionar o código para processar o pagamento com as informações do cartão de crédito
-
-                // Fecha o modal após o envio do formulário
-                $('#cartaoModal').modal('hide');
-                alert('Pagamento sendo processado. Aguarde..');
-
-                // Redireciona para a página de pagamento com cartão de crédito
-                window.location.href = 'pagamento_cartao.php';
-            });
-        });
-    </script>
-
-
 </body>
 
 </html>
